0c4062158e9f9b487317f1ce6aa60192abfc0faf
Mohamed Mortaga <92436959+MohamedMortaga@users.noreply.github.com>
2024-11-28 19:37:31 +0200

    Initial commit

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 89);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Allocation";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 21);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Max";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(526, 89);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Need";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(687, 89);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Available";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.SeaShell;
+            this.richTextBox1.Location = new System.Drawing.Point(50, 136);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(92, 416);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.SeaShell;
+            this.richTextBox2.Location = new System.Drawing.Point(214, 136);
+            this.richTextBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(92, 416);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.BackColor = System.Drawing.Color.SeaShell;
+            this.richTextBox3.Location = new System.Drawing.Point(358, 136);
+            this.richTextBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(92, 416);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.BackColor = System.Drawing.Color.SeaShell;
+            this.richTextBox4.Location = new System.Drawing.Point(531, 136);
+            this.richTextBox4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(92, 416);
+            this.richTextBox4.TabIndex = 7;
+            this.richTextBox4.Text = "";
+            this.richTextBox4.TextChanged += new System.EventHandler(this.richTextBox4_TextChanged);
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.BackColor = System.Drawing.Color.SeaShell;
+            this.richTextBox5.Location = new System.Drawing.Point(692, 136);
+            this.richTextBox5.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(92, 416);
+            this.richTextBox5.TabIndex = 8;
+            this.richTextBox5.Text = "";
+            this.richTextBox5.TextChanged += new System.EventHandler(this.richTextBox5_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(50, 89);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(78, 21);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Processes";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(210, 34);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(132, 21);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Process Number :";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.SeaShell;
+            this.textBox1.Location = new System.Drawing.Point(372, 29);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(55, 28);
+            this.textBox1.TabIndex = 11;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.MintCream;
+            this.button1.Location = new System.Drawing.Point(464, 26);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(162, 37);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Show Process";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.MintCream;
+            this.button2.Location = new System.Drawing.Point(512, 564);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(132, 40);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "Show Need";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.MintCream;
+            this.button3.Location = new System.Drawing.Point(675, 564);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(132, 37);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Show Avaibale";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(54, 599);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 21);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "Numbers of Resources :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.SeaShell;
+            this.textBox2.Location = new System.Drawing.Point(240, 594);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(67, 28);
+            this.textBox2.TabIndex = 16;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.MintCream;
+            this.button4.Location = new System.Drawing.Point(531, 614);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(229, 37);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(867, 698);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Italic | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GUI";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        List<int> LAllocation = new List<int>();
+        List<string> LMax = new List<string>();
+        List<string> LNeed = new List<string>();
+        List<int> LAv = new List<int>();
+        int stop = 0, p = 0;
+
+        string y, v;
+        public Form1()
+        {
+            InitializeComponent();
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+        }
+
+        void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Back:
+                    if (p > 0)
+                    {
+                        p--;
+                    }
+                    break;
+
+            }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string num = textBox1.Text;
+            richTextBox1.Clear();
+
+            for (int i = 0; i < Convert.ToInt16(num); i++)
+            {
+                richTextBox1.AppendText("P" + Convert.ToString(i) + Environment.NewLine);
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+            string num = textBox2.Text;
+            for (int i = 0; i < richTextBox1.Lines.Length - 1; i++)
+            {
+
+                LAllocation.Add(Convert.ToInt16(richTextBox2.Lines[i]));
+                LMax.Add(richTextBox3.Lines[i]);
+
+            }
+
+            for (int i = 0; i < richTextBox1.Lines.Length - 1; i++)
+            {
+                int x = Convert.ToInt16(LMax[i]) - Convert.ToInt16(LAllocation[i]);
+                string need = Convert.ToString(x);
+                v = Convert.ToString(need);
+
+
+                if (need.Length < Convert.ToInt16(num))
+                {
+                    count = Convert.ToInt16(num) - need.Length;
+                    for (int j = 0; j < count; j++)
+                    {
+                        y = '0' + v;
+                        v = y;
+                    }
+                    richTextBox4.AppendText(v + Environment.NewLine);
+                    LNeed.Add(v);
+                }
+                else
+                {
+                    richTextBox4.AppendText(Convert.ToString(need) + Environment.NewLine);
+                    LNeed.Add(need);
+                }
+
+
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void richTextBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int y = 0; y < LAllocation.Count; y++)
+            {
+
+                stop += LAllocation[y];
+
+            }
+            stop += LAv[LAv.Count - 1];
+
+
+            string num = textBox2.Text;
+            List<int> Lresources1 = new List<int>(Convert.ToInt16(num));
+            List<int> Lresources2 = new List<int>(Convert.ToInt16(num));
+
+
+            while (Convert.ToInt16(richTextBox5.Lines[richTextBox5.Lines.Length - 1]) != stop)
+            {
+
+                for (int i = 0; i < richTextBox4.Lines.Length - 1; i++)
+                {
+                    string line2 = Convert.ToString(richTextBox4.Lines[i]);
+                    string line1 = Convert.ToString(richTextBox5.Lines[richTextBox5.Lines.Length - 1]);
+                    for (int g = 0; g < Convert.ToInt16(num); g++)
+                    {
+                        Lresources1.Add(Convert.ToInt16(line1.ToCharArray()[g]));
+                        Lresources2.Add(Convert.ToInt16(line2.ToCharArray()[g]));
+                    }
+
+                    int count = 0;
+                    for (int g = 0; g < Convert.ToInt16(num); g++)
+                    {
+                        if (Lresources1[g] > Lresources2[g])
+                        {
+                            count++;
+                        }
+
+                    }
+
+                    if (count == Convert.ToInt16(num) - 1)
+                    {
+                        int Av = LAllocation[i] + LAv[LAv.Count - 1];
+                        LAv.Add(Av);
+                        richTextBox5.AppendText(Environment.NewLine + Convert.ToString(Av));
+                    }
+
+                }
+
+                for (int k = 0; k < Convert.ToInt16(num); k++)
+                {
+                    Lresources2.RemoveAt(k);
+                    Lresources1.RemoveAt(k);
+                }
+            }
+        }
+
+        private void richTextBox5_TextChanged(object sender, EventArgs e)
+        {
+            p++;
+            if (p == Convert.ToInt16(textBox2.Text))
+            {
+                for (int i = 0; i < richTextBox5.Lines.Length; i++)
+                {
+                    LAv.Add(Convert.ToInt16(richTextBox5.Lines[i]));
+
+                }
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+            richTextBox2.Clear();
+            richTextBox3.Clear();
+            richTextBox4.Clear();
+            richTextBox5.Clear();
+            LAv.Clear();
+            LAllocation.Clear();
+            LMax.Clear();
+            LNeed.Clear();
+            textBox1.Text = null;
+            textBox2.Text = null;
+            stop = 0;
+            p = 0;
+            y = null;
+            v=null;
+        }
+    }
+}
